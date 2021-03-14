3203d088f54c0823bc98a722902bfaafd7e4c0d5
Naomi Calabretta <me@arytonex.pw>
2021-03-14 17:59:06 +0100

    sdm660-common: Support POSP and POSP-based ROMs

---
+++ smartbuild/potato-common/sdm660.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Product/board variable separated ROMs require the following set of flags
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := sdm660
+
+# Power 
+PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock
